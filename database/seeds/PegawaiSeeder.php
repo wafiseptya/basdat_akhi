696a0d53245f06b67686cd87a10aeb07f08a8b15
Wafi <septya.wafi@gmail.com>
2019-11-29 01:43:45 +0700

    pegawai seeder update

--- database/seeds/PegawaiSeeder.php
+++ database/seeds/PegawaiSeeder.php
@@ -16,12 +16,24 @@ class PegawaiSeeder extends Seeder
 
         $faker = Faker::create('id_ID');
 
-        for($i=0; $i<50; $i++){
+        DB::table('pegawai')->insert([
+            'nip' => $faker->date($format = 'Ymd', $max = 'now').$faker->date($format = 'Ym', $max = 'now').$faker->numberBetween($min = 1, $max = 2).str_pad(0, 3, '0', STR_PAD_LEFT),
+            'name_pegawai' =>$faker->name,
+            'pangkat' => 'IVe',
+            'jabatan' => 'Kepala Desa',
+            'instansi' => 'Desa '.$faker->company,
+            'updated_by'=>$this->getID()
+        ]);
+
+        for($i=1; $i<50; $i++){
+            $nip = $faker->date($format = 'Ymd', $max = 'now').$faker->date($format = 'Ym', $max = 'now').$faker->numberBetween($min = 1, $max = 2).str_pad($i, 3, '0', STR_PAD_LEFT);
+            $pangkat = $faker->randomElement($array = array ('I','II','III','IV')).$faker->randomElement($array = array ('a','b','c','d'));
+            $jabatan = $faker->randomElement($array = array ('Pegawai','Sekretaris','Pelaksana','PBD','Kaur Pem','Kaur PBD','Kaur Peb','Kaur Kesra','Kaur Keuangan','Kaur Umum','Pelaksana Teknis','Pelaksana Wilayah','Pembantu Desa'));
             DB::table('pegawai')->insert([
-                'nip' => $faker->randomNumber,
+                'nip' => $nip,
                 'name_pegawai'  =>$faker->name,
-                'pangkat' =>$faker->word,
-                'jabatan' =>$faker->jobTitle,
+                'pangkat' =>$pangkat,
+                'jabatan' =>$jabatan,
                 'instansi' =>$faker->company,
                 'updated_by'=>$this->getID()
             ]);
