91fbda8ecdee8019badcd972d9589903cdec36b2
Wafi <septya.wafi@gmail.com>
2019-11-28 19:54:23 +0700

    seeding logs

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(KabupatenTableSeeder::class);
-        $this->call(KendaraanSeeder::class);
-        $this->call(mataAnggaran::class);
-        $this->call(WaktuPerjalanan::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(KabupatenTableSeeder::class);
+        // $this->call(KendaraanSeeder::class);
+        // $this->call(mataAnggaran::class);
+        // $this->call(WaktuPerjalanan::class);
+        // $this->call(PembebananAnggaranSeeder::class);
+        // $this->call(PegawaiSeeder::class);
+        $this->call(LogsSeeder::class);
     }
 }
