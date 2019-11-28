1f9dd8ebddd2c62f44ed52fec32c2cf0952db600
barismar <barisma99@gmail.com>
2019-11-28 15:59:52 +0700

    add seeder

---
+++ database/seeds/KendaraanSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Factory as Faker;
+
+class KendaraanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+
+        $kendaraan = ['Pesawat','Mobil','Sepeda Motor', 'Kapal', 'Bus', 'Kereta Api'];
+        
+    	for($i = 0; $i < count($kendaraan); $i++){
+    		DB::table('kendaraan')->insert([
+    			'jenis' => $kendaraan[$i]
+    		]);
+    	}
+    }
+}
